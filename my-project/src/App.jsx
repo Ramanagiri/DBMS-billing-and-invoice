68fb10d1973db7d53c5020c26482a3b68aff3439
ramanagiri <ra3451@srmist.edu.in>
2023-07-28 09:50:55 +0530

    3rd

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Darkmode from './components/Darkmode'
-import TopNavbar from './components/TopNavbar'
 
 export default function App() {
   return (
     <>
-    <div className="backgound  bg-cyan-100 mb-3 text-gray-500 dark:text-gray-400 ">hello</div>
-    {/* <Darkmode/> */}
-    <TopNavbar/>
+    hello
 
     </>
   )
